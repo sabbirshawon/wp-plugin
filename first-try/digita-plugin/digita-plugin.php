ab9a4f54d4cf74e8dd5af52491e9f76aaa89fc8d
Sabbir Ahmed <sabbirs93@gmail.com>
2018-11-06 16:05:32 +0600

    Add files via upload

---
+++ first-try/digita-plugin/digita-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @package DigitaPlugin
+
+*/
+
+/*
+Plugin Name: Digita Plugin
+Plugin URI: http://digitainteractive.com/plugin
+Description: This is my first attemt on writng a custom Plugin for Digita Interactive
+Version: 1.0.0
+Author: "Sabbir Shawon"
+Author URI: https://sabbirshawon.github.io/
+License: GPLv2 or Later
+Text Domain: Digita-Plugin
+
+*/

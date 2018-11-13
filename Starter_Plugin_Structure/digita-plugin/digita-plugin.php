5209bdf0a7c3f62c5c5f25d310e7ff57bfc318fb
Sabbir Ahmed <sabbirs93@gmail.com>
2018-11-13 18:29:25 +0600

    Add files via upload

---
+++ Starter_Plugin_Structure/digita-plugin/digita-plugin.php
@@ -0,0 +1,70 @@
+<?php
+/**
+* @package DigitaPlugin
+*/
+/*
+Plugin Name: Digita Plugin
+Plugin URI: http://digitainteractive.com/plugin
+Description: This is my first attemt on writng a custom Plugin for Digita Interactive
+Version: 1.0.0
+Author: "Sabbir Shawon"
+Author URI: https://sabbirshawon.github.io/
+License: GPLv2 or Later
+Text Domain: Digita-Plugin
+*/
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+Copyright 2005-2018 Automattic, Inc
+*/
+//	if ( ! defined('ABSPATH')){	
+//		die;
+//	}
+defined ('ABSPATH') or die ('Hey, you can\t access this file, you silly human!');
+/*if (! function_exists('add_action')){
+	echo 'Hey, you can\t access this file, you silly human!';
+	exit;
+}*/
+if ( file_exists( dirname(__FILE__).'/vendor/autoload.php' ) ){
+	require_once dirname(__FILE__).'/vendor/autoload.php';
+}
+
+define('PLUGIN_PATH', plugin_dir_path( __FILE__ ));
+define('PLUGIN_URL', plugin_dir_url( __FILE__ ));
+define('PLUGIN', plugin_basename( __FILE__ ));
+
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
+/**
+ * The code that runs during plugin activation
+ */
+
+function activate_digita_plugin(){
+	Activate::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation
+ */
+
+function deactivate_digita_plugin(){
+	Deactivate::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_digita_plugin');
+register_activation_hook( __FILE__, 'deactivate_digita_plugin');
+
+
+if( class_exists( 'Inc\\Init' ) ){
+	Inc\Init::register_services(); 
+}

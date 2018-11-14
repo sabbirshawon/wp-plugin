2f80d29d1dc481fa49253e114bc0955f4d1bcbd9
Sabbir Ahmed <sabbirs93@gmail.com>
2018-11-14 15:24:39 +0600

    Add files via upload

---
+++ Modular_Admin_SubPages/digita-plugin/inc/Pages/Admin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Inc\Pages;
+
+
+use \Inc\Base\BaseController;
+
+use \Inc\Api\SettingsApi;
+
+/**
+* 
+*/
+
+class Admin extends BaseController
+{
+	public $settings;
+
+	public $pages = array();
+
+	public $subpages = array();
+
+	public function __construct(){
+
+		$this->settings = new SettingsApi();
+
+		$this->pages = array(
+			array(
+				'page_title' => 'Digita Plugin', 
+				'menu_title' => 'Digita', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'digita_plugin', 
+				'callback' => function() { echo '<h1>Digita Plugin</h1>'; }, 
+				'icon_url' => 'dashicons-store', 
+				'position' => 110
+			)
+		);
+
+		$this->subpages = array(
+			array(
+				'parent_slug' => 'digita_plugin', 
+				'page_title' => 'Custom Post Types', 
+				'menu_title' => 'CPT', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'digita_cpt', 
+				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+			),
+			array(
+				'parent_slug' => 'digita_plugin', 
+				'page_title' => 'Custom Taxonomies', 
+				'menu_title' => 'Taxonomies', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'digita_taxonomies', 
+				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+			),
+			array(
+				'parent_slug' => 'digita_plugin', 
+				'page_title' => 'Custom Widgets', 
+				'menu_title' => 'Widgets', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'digita_widgets', 
+				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+			)
+		);
+
+	}
+
+	public function register() 
+	{
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
+	}
+}
